07802ba98f76b24f3026cb40b1a038dddfb933d5
MikiWojak <mikizarowa8@gmail.com>
2021-03-16 18:39:35 +0100

    Show users - backend
    - Show list of users
    - Add UserController
    - Code route
    - Only on backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//For backend only
+Route::get('/users', 'App\Http\Controllers\UserController@index');
