83f186202c2d8efc5b3eb0e8973906a26ba84ca9
MikiWojak <mikizarowa8@gmail.com>
2021-03-16 19:38:39 +0100

    List of users from API - problems
    - Try to get list of users from Laravel API
    - Error 401 - unauthorized
    - Do not know how to handle authorization token without login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      info: null
+    }
+  },
+
+  mounted() {
+    this.loadInfo();
+  },
+
+  methods: {
+    loadInfo: function() {
+      axios.get('http://localhost:8000/api/user')
+      .then((response) => {
+        this.info = response;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
